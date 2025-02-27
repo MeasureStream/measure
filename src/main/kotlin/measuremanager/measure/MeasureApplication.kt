ded454ed5b7d291e306624f278c0c914075855dc
R3M1d4Cod3r <superchristian9@gmail.com>
2025-02-27 10:46:08 +0100

    init

---
+++ src/main/kotlin/measuremanager/measure/MeasureApplication.kt
@@ -0,0 +1,11 @@
+package measuremanager.measure
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeasureApplication
+
+fun main(args: Array<String>) {
+    runApplication<MeasureApplication>(*args)
+}
---
+++ src/test/kotlin/measuremanager/measure/MeasureApplicationTests.kt
@@ -0,0 +1,13 @@
+package measuremanager.measure
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MeasureApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
