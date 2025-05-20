fd9e25988a4919c107c1a8058028d95178415d9c
R3M1d4Cod3r <superchristian9@gmail.com>
2025-05-20 11:10:52 +0200

    fix: dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
+
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
     id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
 }
+
 
 group = "measuremanager"
 version = "0.0.1-SNAPSHOT"
