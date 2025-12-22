f35d9acfea62789455e095dba09dd9df2fe08ae0
christian <superchristian9@gmail.com>
2025-12-22 21:21:17 +0100

    FEAT: removed initialization fake data (it is activated only for dev profile)

--- src/main/kotlin/measuremanager/measure/configurations/DataInitializer.kt
+++ src/main/kotlin/measuremanager/measure/configurations/DataInitializer.kt
@@ -11,53 +11,53 @@ import org.springframework.stereotype.Component
 import java.time.Instant
 import kotlin.random.Random
 
-
 @Component
+@Profile("dev")
 class DataInitializer(
     private val mr: MeasureRepository,
     private val mongoTemplate: MongoTemplate,
-    private val mur : MURepository
-) : InitializingBean{
+    private val mur: MURepository,
+) : InitializingBean {
     @PostConstruct
-    fun init(){
+    fun init() {
         print("\ninitialize fake data\n")
     }
 
     override fun afterPropertiesSet() {
         mongoTemplate.db.drop()
 
-        //val measureUnits = listOf("Celsius", "Kelvin", "Pascal", "Lux")
+        // val measureUnits = listOf("Celsius", "Kelvin", "Pascal", "Lux")
         val measureUnits = listOf("Celsius", "Pascal")
-        //b0be4ea5-17d3-4e63-ad81-510b4532dac8
-
+        // b0be4ea5-17d3-4e63-ad81-510b4532dac8
 
         val now = Instant.now()
         val totalSeconds = 36000L
         val steps = 100
         val interval = totalSeconds / steps
 
-
-        val measures1 = List(100) { i ->
-            Measure().apply {
-                id = null // Lasciato null per essere generato dal database
-                value = Random.nextDouble(18.0, 22.0)
-                measureUnit = measureUnits.random()
-                //time = Instant.now().minusSeconds(Random.nextLong(0, 36000))
-                time = now.minusSeconds(totalSeconds - i * interval)
-                nodeId = 1
+        val measures1 =
+            List(100) { i ->
+                Measure().apply {
+                    id = null // Lasciato null per essere generato dal database
+                    value = Random.nextDouble(18.0, 22.0)
+                    measureUnit = measureUnits.random()
+                    // time = Instant.now().minusSeconds(Random.nextLong(0, 36000))
+                    time = now.minusSeconds(totalSeconds - i * interval)
+                    nodeId = 1
+                }
             }
-        }
 
-        val mus = List(20) { it ->
-            MeasurementUnit().apply {
-                id = null
-                muNetworkId = (it + 1 ).toLong()
-                userId = "b0be4ea5-17d3-4e63-ad81-510b4532dac8"
+        val mus =
+            List(20) { it ->
+                MeasurementUnit().apply {
+                    id = null
+                    muNetworkId = (it + 1).toLong()
+                    userId = "b0be4ea5-17d3-4e63-ad81-510b4532dac8"
+                }
             }
-        }
 
         mr.saveAll(measures1)
         mur.saveAll(mus)
-
     }
 }
+
